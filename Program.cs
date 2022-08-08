2823010de85fcfb48c5d9ee3bbf687355366ed64
Matthew Connley <matt@mattconnley.com>
2022-08-07 20:56:02 -0500

    fixed healthcheck and program.cs logging
    Signed-off-by: Matthew Connley <matt@mattconnley.com>

--- Program.cs
+++ Program.cs
@@ -1,28 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
 
-builder.Configuration.AddJsonFile("secrets/secrets.json", optional: false);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton(builder.Configuration.GetSection("customConfiguration").Get<CustomConfiguration>());
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Configuration.AddJsonFile("secrets/secrets.json", optional: false);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSingleton(builder.Configuration.GetSection("customConfiguration").Get<CustomConfiguration>());
+    builder.Services.AddHealthChecks();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.MapHealthChecks("/healthz");
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    IHostApplicationLifetime lifetime = app.Lifetime;
+
+    lifetime.ApplicationStopping.Register(() =>
+    {
+        Thread.Sleep(5000);
+    });
+
+    app.Run();
+}
+catch (System.Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+}
+finally
+{
+    NLog.LogManager.Shutdown();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
